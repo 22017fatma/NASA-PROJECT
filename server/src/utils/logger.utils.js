30c0ba9524be3ba0266dfc5446f0bb773f4d8975
22017fatma <2201744@student.eelu.edu.eg>
2025-07-25 17:04:09 +0300

    fix(logger): enhance logging to save errors in daily log files refactor: add error classification to distinguish between generic and operational

--- server/src/utils/logger.utils.js
+++ server/src/utils/logger.utils.js
@@ -1,16 +1,24 @@
 
 import fs from 'fs';
 import path from 'path';
+import { AppError } from './AppError.utils.js';
 
 
 function logErrorToFile(err) {
-    const logFilePath=path.join('logs','error.log')
+    // Log error details to a file
+    const data= new Date();
+    const today=data.toISOString().split(':')[0].split('T')[0];
+    const logFilePath = path.join('logs', `${today}.log`);
+
+    const errorType = err instanceof AppError ? 'Operational Error' : 'Programming Error';
     const errorLog = `
+    ErrorType: ${errorType}
     Environment: ${process.env.NODE_ENV}
     Time: ${new Date().toISOString()}
     Name: ${err.name}
     Message: ${err.message}
     Stack: ${err.stack}
+    
     `;
         if (!fs.existsSync('logs')) {
         fs.mkdirSync('logs');
