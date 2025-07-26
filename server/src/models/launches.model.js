9a605bff1a16596b9ee81f42e4109f8925bd874e
22017fatma <2201744@student.eelu.edu.eg>
2025-07-26 06:31:33 +0300

    feat(launch):added new feature to launch model

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,6 @@
 
 const launches=new Map();
+let latestFlightNumber=100;
 const launch={
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -15,8 +16,23 @@ const launch={
 launches.set(launch.flightNumber,launch);
 function getAllLaunches(){
     return Array.from(launches.values());
+
+}
+
+function addNewLaunch(launch){
+  latestFlightNumber++;
+  launches.set(launches.flightNumber,
+    Object.assign(launch,{
+      success:true,
+      upcoming:true,
+      customer:['Zero to Mastry','NASA'],
+      flightNumber:latestFlightNumber,
+
+  })
+);
 }
 export{
     
   getAllLaunches,
+  addNewLaunch
 };
