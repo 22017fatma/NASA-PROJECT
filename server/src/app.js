a0daa15fa08e7856720e9927bbbc200599c980dc
22017fatma <2201744@student.eelu.edu.eg>
2025-07-26 05:19:39 +0300

    fix(package): fixed watch and correct morgan fix(app): correct import statement

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,43 @@
 'use strict';
 import express from 'express';
+import morgan from 'morgan';
 import cors from 'cors'; 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import{ planetsRouter } from './routes/planets/planets.router.js';
-import {  globalErrorHandler } from './middlewares/customError.middleware.js';
+import { launchesRouter } from './routes/launches/launches.router.js';
 import { AppError } from './utils/AppError.utils.js';
 import dotenv from 'dotenv';   
+import { globalErrorHandler } from './utils/errorHandler.utils.js';
 dotenv.config();
 const app = express();
+
+app.use(morgan('combined'));// Logging middleware 
 
 app.use(cors({
     
     origin: '*' // Adjust the origin as needed 
 }));
-app.use(express.json());
-app.use('/api', planetsRouter);
 
-app.use('*route', (req, _res, _next) => {
-    // Handle all undefined routes
-    throw new AppError(`Can't find ${req.originalUrl} on this server!`, 404)
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
+
+app.use('/api', planetsRouter);
+app.use('/api/launches',launchesRouter);
+
+app.get('/*route',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
 });
+
+// app.use('*route', (req, _res, _next) => {
+//     // Handle all undefined routes
+//     throw new AppError(`Can't find ${req.originalUrl} on this server!`, 404)
+// });
 
 app.use(globalErrorHandler); 
 export {
